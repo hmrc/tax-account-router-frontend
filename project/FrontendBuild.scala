28b46b06ba7247cce260555172baae3a30f0672b
Paolo Boni <paolo.boni@digital.hmrc.gov.uk>
2015-09-16 15:39:39 +0100

    [@paoloboni + @jakobgrunig] Updated dependencies version to the latest.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -18,21 +18,21 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "1.2.1",
-    "uk.gov.hmrc" %% "play-config" % "1.2.0",
-    "uk.gov.hmrc" %% "play-json-logger" % "1.0.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "2.0.0",
+    "uk.gov.hmrc" %% "play-config" % "2.0.0",
+    "uk.gov.hmrc" %% "play-json-logger" % "2.1.0",
     "uk.gov.hmrc" %% "play-health" % "1.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "3.0.0",
-    "uk.gov.hmrc" %% "play-ui" % "3.0.0"
+    "uk.gov.hmrc" %% "govuk-template" % "4.0.0",
+    "uk.gov.hmrc" %% "play-ui" % "4.0.0"
   )
 
   abstract class TestDependencies(scope: String) {
     lazy val test : Seq[ModuleID] = Seq(
-      "org.scalatest" %% "scalatest" % "2.2.2" % scope,
-      "org.pegdown" % "pegdown" % "1.4.2" % scope,
-      "org.jsoup" % "jsoup" % "1.7.3" % scope,
+      "org.scalatest" %% "scalatest" % "2.2.4" % scope,
+      "org.pegdown" % "pegdown" % "1.5.0" % scope,
+      "org.jsoup" % "jsoup" % "1.8.3" % scope,
       "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-      "uk.gov.hmrc" %% "hmrctest" % "1.0.0" % scope
+      "uk.gov.hmrc" %% "hmrctest" % "1.4.0" % scope
     )
   }
 
