b4ecb80ac9b820717ef62817d081a80a2e5230da
Paolo Boni <paolo.boni@digital.hmrc.gov.uk>
2015-10-28 11:39:37 +0000

    Revert "TAR-19 Turned all the meters into histograms."
    This reverts commit 1a287a8acf2be99413b665386cf5e02fc06d999c.

--- test/controllers/RouterControllerSpec.scala
+++ test/controllers/RouterControllerSpec.scala
@@ -16,7 +16,7 @@
 
 package controllers
 
-import com.codahale.metrics.{Histogram, MetricRegistry}
+import com.codahale.metrics.{Meter, MetricRegistry}
 import engine.{Condition, Rule, RuleEngine, When}
 import helpers.SpecHelpers
 import model.Location._
@@ -167,7 +167,7 @@ object Mocks extends MockitoSugar {
     val mockMetricRegistry: MetricRegistry = mock[MetricRegistry]
     val mockMonitoringMetrics: MonitoringMetrics = mock[MonitoringMetrics]
     when(mockMonitoringMetrics.registry).thenReturn(mockMetricRegistry)
-    when(mockMetricRegistry.histogram(anyString())).thenReturn(mock[Histogram])
+    when(mockMetricRegistry.meter(anyString())).thenReturn(mock[Meter])
     mockMonitoringMetrics
   }
 }
